99b4cba066aa7fe51d951ad77516dcee0ad93879
Jojo <jonas.uliana@passwd.com.br>
2020-05-21 01:46:32 -0300

    Added env command line option

--- src/CredentialCat.Console/App.cs
+++ src/CredentialCat.Console/App.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.CommandLine;
 using System.Threading.Tasks;
+using System.CommandLine.Invocation;
+
+using static System.Console;
 
 using CredentialCat.Shared.Entities;
 
 namespace CredentialCat.Console
 {  internal static class App
     {
-        private static async Task Main()
+        private static async Task<int> Main(string[] args)
         {
             var home = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CREDENTIAL_CAT_HOME"))
                 ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".credential_cat")
@@ -17,6 +21,8 @@ namespace CredentialCat.Console
             var configurationFile = Path.Combine(home ?? throw new NullReferenceException(nameof(home)), "configuration.json");
 
             ConfigurationEntity configuration;
+
+            #region Startup validations
 
             if (!Directory.Exists(home))
             {
@@ -30,6 +36,51 @@ namespace CredentialCat.Console
                 var content = JsonSerializer.Serialize(configuration);
                 await File.WriteAllTextAsync(configurationFile, content);
             }
+
+            #endregion
+
+            #region Application comands
+
+
+
+            #endregion
+
+            #region Application options
+
+            static Option ApplicationEnvironment()
+            {
+                var statementOption = new Option(new[] { "-e", "--env" })
+                {
+                    Description = "Show application environment variables"
+                };
+
+                return statementOption;
+            }
+
+            #endregion
+
+            var commands = new RootCommand
+            {
+                ApplicationEnvironment()
+            };
+
+            commands.Handler = CommandHandler.Create<bool>(env =>
+            {
+                if (env)
+                {
+
+                    WriteLine("[+] Application environment variables:");
+
+                    var envHome = Environment.GetEnvironmentVariable("CREDENTIAL_CAT_HOME");
+
+                    WriteLine(string.IsNullOrEmpty(envHome)
+                        ? $" > CREDENTIAL_CAT_HOME is empty, default value in use: {home}"
+                        : $" > CREDENTIAL_CAT_HOME: {envHome}");
+                }
+            });
+
+            commands.Description = "credential-cat help you to enumerate leaked credentials on several sources around the dark web (also in surface).";
+            return await commands.InvokeAsync(args).ConfigureAwait(true);
         }
     }
 }
