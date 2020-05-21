eb7f9e2c93b72fea5930e6844d2c731b859965c6
Jojo <jonas.uliana@passwd.com.br>
2020-05-21 02:49:25 -0300

    Added DatabaseContext

---
+++ src/CredentialCat.Shared/DatabaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CredentialCat.Shared
+{
+    /// <summary>
+    /// Represent current application cache SQlite database instance
+    /// </summary>
+    public class DatabaseEntity : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DatabaseEntity(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_connectionString);
+        }
+    }
+}
