41d597ec1e8ba74a9086d65fb1c39cc6a1c89360
Jojo <jonas.uliana@passwd.com.br>
2020-05-21 01:20:01 -0300

    Added credential-cat environment variable name

--- src/CredentialCat.Console/App.cs
+++ src/CredentialCat.Console/App.cs
@@ -1,9 +1,17 @@
-﻿namespace CredentialCat.Console
+﻿using System;
+using System.IO;
+
+namespace CredentialCat.Console
 {  internal static class App
     {
         private static void Main()
         {
+            var home = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CREDENTIAL_CAT_HOME"))
+                ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".credential_cat")
+                : Environment.GetEnvironmentVariable("CREDENTIAL_CAT_HOME");
 
+            if (!Directory.Exists(home))
+                Directory.CreateDirectory(home);
         }
     }
 }
