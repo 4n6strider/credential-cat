1c91668bfd63ec5a9572058c838e7a284f381c3e
Jojo <jonas.uliana@passwd.com.br>
2020-05-21 01:47:48 -0300

    Added SourceEnum

---
+++ src/CredentialCat.Shared/Enums/SourceEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace CredentialCat.Shared.Enums
+{
+    /// <summary>
+    /// Enumerate available application sources for leak enumeration
+    /// </summary>
+    public enum SourceEnum
+    {
+        Pwndb
+    }
+}
